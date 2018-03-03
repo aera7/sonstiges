c08e14a0fbcb7505b4c1b16079695ae9e08f73ef
fabricio <fabricio.arendtorres@unibas.ch>
2018-03-03 20:17:53 +0100

    removed unnecessary files

--- function_holder.R
+++
@@ -1,9 +0,0 @@
-connection <- function() {
-  con <- dbConnect(RMySQL::MySQL(),
-                   dbname = "retrolan_newkirk",
-                   host = "ams30.siteground.eu",
-                   port = 3306,
-                   user =  "retrolan_gate",
-                   password = "sebastian1990")
-  return(con)
-}
