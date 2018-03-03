2ac7ef501571005a964b00c59643f5f2f2055611
fabricio <fabricio.arendtorres@unibas.ch>
2018-03-03 18:04:16 +0100

    new structure for queries

--- xml_querries.R
+++ xml_querries.R
@@ -1,94 +1,58 @@
-rr = paste('<?xml version="1.0" encoding="UTF-8"?>',
-           '<findCompletedItemsRequest xmlns=\"http://www.ebay.com/marketplace/search/v1/services\">',
-           "<keywords>iPhone</keywords>",
-           "<categoryId>9355</categoryId>",
-           "<itemFilter>",
-           "<name>Condition</name>",
-           "<value>1000</value>",
-           "</itemFilter>",
-           "<itemFilter>",
-           "<name>FreeShippingOnly</name>",
-           "<value>true</value>",
-           "</itemFilter>",
-           "<itemFilter>",
-           "<name>SoldItemsOnly</name>",
-           "<value>true</value>",
-           "</itemFilter>",
-           "<sortOrder>PricePlusShippingLowest</sortOrder>",
-           "<paginationInput>",
-           "<entriesPerPage>2</entriesPerPage>",
-           "<pageNumber>1</pageNumber>",
-           "</paginationInput>",
-           "</findCompletedItemsRequest>",
-           sep = "")
+queries = list()
 
 
 
-paper_mario = paste('<?xml version="1.0" encoding="UTF-8"?>',
-                    '<findCompletedItemsRequest xmlns=\"http://www.ebay.com/marketplace/search/v1/services\">',
-                    "<keywords>iPhone</keywords>",
-                    "<categoryId>9355</categoryId>",
-                    "<itemFilter>",
-                    "<name>Condition</name>",
-                    "<value>1000</value>",
-                    "</itemFilter>",
-                    "<itemFilter>",
-                    "<name>FreeShippingOnly</name>",
-                    "<value>true</value>",
-                    "</itemFilter>",
-                    "<itemFilter>",
-                    "<name>SoldItemsOnly</name>",
-                    "<value>true</value>",
-                    "</itemFilter>",
-                    "<sortOrder>PricePlusShippingLowest</sortOrder>",
-                    "<paginationInput>",
-                    "<entriesPerPage>2</entriesPerPage>",
-                    "<pageNumber>1</pageNumber>",
-                    "</paginationInput>",
-                    "</findCompletedItemsRequest>",
-                    sep = "")
+queries["paper_mario"] <-
+  paste(
+    '<?xml version="1.0" encoding="UTF-8"?>',
+    '<findCompletedItemsRequest xmlns=\"http://www.ebay.com/marketplace/search/v1/services\">',
+    "<keywords>iPhone</keywords>",
+    "<categoryId>9355</categoryId>",
+    "<itemFilter>",
+    "<name>Condition</name>",
+    "<value>1000</value>",
+    "</itemFilter>",
+    "<itemFilter>",
+    "<name>FreeShippingOnly</name>",
+    "<value>true</value>",
+    "</itemFilter>",
+    "<itemFilter>",
+    "<name>SoldItemsOnly</name>",
+    "<value>true</value>",
+    "</itemFilter>",
+    "<sortOrder>PricePlusShippingLowest</sortOrder>",
+    "<paginationInput>",
+    "<entriesPerPage>2</entriesPerPage>",
+    "<pageNumber>1</pageNumber>",
+    "</paginationInput>",
+    "</findCompletedItemsRequest>",
+    sep = ""
+  )
 
 
-diddy_kong_racing = paste('<?xml version="1.0" encoding="UTF-8"?>',
-                          '<findCompletedItemsRequest xmlns=\"http://www.ebay.com/marketplace/search/v1/services\">',
-                          "<keywords>iPhone</keywords>",
-                          "<categoryId>9355</categoryId>",
-                          "<itemFilter>",
-                          "<name>Condition</name>",
-                          "<value>1000</value>",
-                          "</itemFilter>",
-                          "<itemFilter>",
-                          "<name>FreeShippingOnly</name>",
-                          "<value>true</value>",
-                          "</itemFilter>",
-                          "<itemFilter>",
-                          "<name>SoldItemsOnly</name>",
-                          "<value>true</value>",
-                          "</itemFilter>",
-                          "<sortOrder>PricePlusShippingLowest</sortOrder>",
-                          "<paginationInput>",
-                          "<entriesPerPage>2</entriesPerPage>",
-                          "<pageNumber>1</pageNumber>",
-                          "</paginationInput>",
-                          "</findCompletedItemsRequest>",
-                          sep = "")
-
-
-
-
-<?xml version="1.0" encoding="UTF-8"?>
-  <findCompletedItemsRequest xmlns="http://www.ebay.com/marketplace/search/v1/services">
-  <keywords>banjo tooie</keywords>
-  <categoryId>139973</categoryId>
-  <itemFilter>
-  <name>SoldItemsOnly</name>
-  <value>true</value>
-  </itemFilter>
-  <sortOrder>PricePlusShippingLowest</sortOrder>
-  <paginationInput>
-  <entriesPerPage>15</entriesPerPage>
-  <pageNumber>1</pageNumber>
-  </paginationInput>
-  </findCompletedItemsRequest>
-  
-  
+queries["diddy_kong_racing"] <-
+  paste(
+    '<?xml version="1.0" encoding="UTF-8"?>',
+    '<findCompletedItemsRequest xmlns=\"http://www.ebay.com/marketplace/search/v1/services\">',
+    "<keywords>iPhone</keywords>",
+    "<categoryId>9355</categoryId>",
+    "<itemFilter>",
+    "<name>Condition</name>",
+    "<value>1000</value>",
+    "</itemFilter>",
+    "<itemFilter>",
+    "<name>FreeShippingOnly</name>",
+    "<value>true</value>",
+    "</itemFilter>",
+    "<itemFilter>",
+    "<name>SoldItemsOnly</name>",
+    "<value>true</value>",
+    "</itemFilter>",
+    "<sortOrder>PricePlusShippingLowest</sortOrder>",
+    "<paginationInput>",
+    "<entriesPerPage>2</entriesPerPage>",
+    "<pageNumber>1</pageNumber>",
+    "</paginationInput>",
+    "</findCompletedItemsRequest>",
+    sep = ""
+  )
