c0f5dc76e632fff52e62e0364aac3da737dc4f52
liquidoxygen02 <gametopia37@gmail.com>
2018-03-09 02:19:32 +0100

    Succesful Parser for images added

--- get_image_collection.R
+++ get_image_collection.R
@@ -5,16 +5,53 @@ library(XML)
 link <- "http://www.ebay.de/itm/Paper-Mario-Nintendo-64-N64-Top-/112754779347"
 
 html <- getURL(link, followlocation = TRUE)
-doc2<- gsub("[[:space:]]", "", html)
+doc<- gsub("[[:space:]]", "", html)
+#doc = htmlParse(doc, asText=TRUE)
 
-pos = regexpr('tdclass="imgimg140">', doc2)
+pos = regexpr('tdclass="imgimg140">', doc)
+x2<-substr(doc, pos[1]+attr(pos, "match.length")+8, nchar(doc))
+pos = regexpr('style="display', x2)
+link2<-substr(x2, 1,pos[1]-2)
+
+html2 <- getURL(link2, followlocation = TRUE)
+doc2<- gsub("[[:space:]]", "", html2)
+
+pos =  regexpr('ebay.viewItem.imageCarousel', doc2)
+pos2 = regexpr('ebay.viewItem.enlargeLayerv2', doc2)
+
+temp_str=  substr(doc2,pos[1],pos2[1])
+
+link_list = c('')
+repeat{
+  posi = regexpr('s-l1600', temp_str)
+  if(posi[1]==-1){
+    break
+  }
+  link_list = c(link_list, substr(temp_str, posi[1]-17,posi[1]-2) )
+  temp_str = substr(temp_str, posi[1]+20, nchar(temp_str))
+}
+
+lapply(link_list, function(x) print(paste0("https://i.ebayimg.com/images/g/",x,"/s-l1600.jpg")))
+#### TO BE UPLOADED INTO DB RUNNING FUNCTION WHILE IMPORTING?
+### HOW to make sure no connection issue raises.
+
+
+#pos = regexpr('ebay.viewItem.imageCarousel', temp_str)
+
+  
+  
+  
+
+write(temp_str, file = "data",append = FALSE, sep = " ")
+
 x2<-substr(doc2, pos[1]+attr(pos, "match.length")+8, nchar(doc2))
 pos = regexpr('style="display', x2)
 link2<-substr(x2, 1,pos[1]-2)
 
-link2
 
-html2 <- getURL(link2, followlocation = TRUE)
+
+yn8AAOSwi4laW2Dq
+
 
 # to be implemented => for loop for remaining pictures
  
