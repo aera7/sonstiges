efc0485d916d16704da2e259ff18591e5c9a3ea1
liquidoxygen02 <gametopia37@gmail.com>
2018-03-25 23:09:19 +0200

    First item successful listed with token on ebay.com sandbox => used user: TESTUSER_leonhardusa

---
+++ item_to_be_listed.R
@@ -0,0 +1,53 @@
+
+
+
+
+X-EBAY-API-SITEID:0
+X-EBAY-API-COMPATIBILITY-LEVEL:967
+X-EBAY-API-CALL-NAME:AddItem
+X-EBAY-API-DEV-NAME:Sebastia-dreamgam-SBX-208f655c9-9baa824
+
+<?xml version="1.0" encoding="utf-8"?>
+  <AddItemRequest xmlns="urn:ebay:apis:eBLBaseComponents">
+  <RequesterCredentials> <eBayAuthToken>AgAAAA**AQAAAA**aAAAAA**lQW4Wg**nY+sHZ2PrBmdj6wVnY+sEZ2PrA2dj6wFk4ahCZeCpgudj6x9nY+seQ**7DAEAA**AAMAAA**xZFlJtAeCDGxPuupER0PtaIe28nzCZ+AmW2at4MG0qFYlsRaajFxdP7+H/NXuQvNvjEfgwg7G8c/EXx8bnbsQPgR6/jbxI3eRgmzlOisXVAe9C6pwKNt8In559J1GW8yUiBXmSoNRgM3Z20q23HXqhdlXqRZWtopUwTsEzc7M1jDDq6sE6XMOf3KZRIggzHl9Ch5IjnP9e84/ZKOxSYkTRGk1o8DX8r6WA7SqTbuFnYGuS1OdldaoawArudJ5r0IC/pCRL7iPd/MLWYnHwrawMtRJtDzMbzf7h2aurjPQI8Fo6X3H3/38dD8CfebbQ7+eLem5bQfP9m/F2FsHIe6OF7iHY0hzgA77IGtUFP87IlKapjXwmg8J0+04vKm+lQ9XEb4II8asu4FPRa/DOlF2wanfd6D6q60Ptjupz4AH5HHiVKkBgX9ZWaOI+gd04h4XRRur+3fC39BHboS+KthQKaGy9iGLtebsUbpE03wD7K/2od/Jdxv8hoQ9WRTfLhzBR/wskhRDUoF5hUDwcx2hpSzcgt8AjY1J19zDFr/51sd6tUqjF7zqQ+ujBXRrlFbTMpkmbiMTNLguT0x4ek5x/zwxdqLh5s73vgofcziJARGBJZlUa9pSI6jI9vf9nzlEr9EiVD7bgM+vva2XiJ50uWZiieNtLDrRjUWpbogiMtod0J5zqcNsFjlej+kg9/6Gol2XATvLFsPJuBI7QlMCHGVEFS2FZgWUR5ce+objXQnSTsUfMJI4SUOxYKtMvF3</eBayAuthToken>
+  </RequesterCredentials>
+  <ErrorLanguage>en_US</ErrorLanguage>
+  <WarningLevel>High</WarningLevel>
+  <Item>
+  <Title>Harry Potter and the Philosopher's Stone</Title>
+<Description>This is the first book in the Harry Potter series. In excellent condition!</Description>
+<PrimaryCategory>
+<CategoryID>377</CategoryID>
+</PrimaryCategory>
+<StartPrice>1.0</StartPrice>
+<CategoryMappingAllowed>true</CategoryMappingAllowed>
+<Country>US</Country>
+<Currency>USD</Currency>
+<ConditionID>1000</ConditionID>
+<DispatchTimeMax>3</DispatchTimeMax>
+<ListingDuration>Days_7</ListingDuration>
+<ListingType>Chinese</ListingType>
+<PaymentMethods>CashOnPickup</PaymentMethods>
+<PictureDetails>
+<PictureURL>http://pics.ebay.com/aw/pics/dot_clear.gif</PictureURL>
+</PictureDetails>
+<PostalCode>95125</PostalCode>
+<Quantity>1</Quantity>
+<ReturnPolicy>
+<ReturnsAcceptedOption>ReturnsAccepted</ReturnsAcceptedOption>
+<RefundOption>MoneyBack</RefundOption>
+<ReturnsWithinOption>Days_30</ReturnsWithinOption>
+<Description>If you are not satisfied, return the book for refund.</Description>
+<ShippingCostPaidByOption>Buyer</ShippingCostPaidByOption>
+</ReturnPolicy>
+<ShippingDetails>
+<ShippingType>Flat</ShippingType>
+<ShippingServiceOptions>
+<ShippingServicePriority>1</ShippingServicePriority>
+<ShippingService>USPSMedia</ShippingService>
+<ShippingServiceCost>2.50</ShippingServiceCost>
+</ShippingServiceOptions>
+</ShippingDetails>
+<Site>US</Site>
+</Item>
+</AddItemRequest>
